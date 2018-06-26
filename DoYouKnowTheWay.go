1d6636ca33bc603d6de9afde051b5a30111103f5
frnkthtnk100 <fpettigrosso@holyfamily.edu>
2018-06-25 22:40:55 -0400

    got rid of text and change it to the more appropriate name of usename make it as if we know the way

--- DoYouKnowTheWay.go
+++ DoYouKnowTheWay.go
@@ -2,11 +2,19 @@ package main
 import ("fmt"
 	"bufio"
 	"os"
+	"strings"
 	)
 func main () {
 	reader := bufio.NewReader(os.Stdin)
 	fmt.Println("Hello, what is your name?")
-	text, _ := reader.ReadString('\n')
-	fmt.Println(text)
+	username, _ := reader.ReadString('\n')
+	fmt.Println("hello",username,"do you know the way?[yes/no]")
+	option, _   := reader.ReadString('\n')
+	if strings.Replace(option,"\n","",-1)  ==  "yes" {
+	fmt.Println("Oh you do know the way. Tell me the way!")
+	}else{
+	fmt.Println("but you do know the way!")
+	}
+	
 
 }
